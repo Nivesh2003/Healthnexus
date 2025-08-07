1d751f410fde7b83b1f9176055baa7027f0f2175
Nivesh2003 <niveshchaudhary3009@gmail.com>
2025-08-07 13:27:50 +0530

    Created and linked login and register page , as well as made a nav item for the redirect

---
+++ frontend/src/Components/Authpage.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { NavLink, useLocation } from 'react-router-dom';
+import LoginForm from '../Elements/LoginForm';
+import RegisterForm from '../Elements/RegisterForm';
+function Authpage() {
+    const location = useLocation();
+  return (
+    <div className="container mt-5">
+    {/* Selection Tabs  */}
+    <div className="d-flex justify-content-center mb-3 border-bottom">
+      <NavLink to="/login" className="nav-link px-4" style={({ isActive }) => ({
+        borderBottom: isActive ? '2px solid #00bcd4' : 'none',
+        color: isActive ? '#00bcd4' : 'black',
+        fontWeight: 'bold'
+      })}>
+        Login
+      </NavLink>
+      <NavLink to="/register" className="nav-link px-4" style={({ isActive }) => ({
+        borderBottom: isActive ? '2px solid #00bcd4' : 'none',
+        color: isActive ? '#00bcd4' : 'black',
+        fontWeight: 'bold'
+      })}>
+        Register
+      </NavLink>
+    </div>
+    {/* Selection Tabs khtm */}
+
+    {/* Content layout */}
+    <div className="container">
+  <div className="row justify-content-center align-items-center mt-5">
+    {/* Left image area */}
+    <div className="col-md-5 d-none d-md-block">
+      <img
+        src="https://accounts.practo.com/static/images/illustration.png"
+        alt="Login Visual"
+        className="img-fluid" 
+      />
+    </div>
+
+    {/* Right form area */}
+    <div className="col-md-5 col-sm-12">
+    {location.pathname === '/login' && <LoginForm />}
+    {location.pathname === '/register' && <RegisterForm />}
+    </div>
+  </div>
+</div>
+
+  </div>
+  );
+}
+
+export default Authpage
---
+++ frontend/src/Elements/RegisterForm.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+ function RegisterForm() {
+  return (
+    <div className="p-4 border rounded shadow-sm bg-white">
+      <div className="mb-3">
+        <h4 className='text-info'>Join HealthNexus</h4>
+        <p className="mb-0"> Are you a doctor?
+       <a href="" style={{ color: "#f90", textDecoration: "none" }}> Register Here </a> </p>
+      </div>
+      <hr />
+      <form>
+      <div className="mb-3">
+        <label className="form-label">Full Name</label>
+        <input type="text" className="form-control" placeholder="Full Name" style={{ fontWeight: 200 }}/>
+      </div>
+
+      <div className="mb-3">
+        <label className="form-label">Date of Birth</label>
+        <input type="date" className="form-control" placeholder="mm/dd/yyy" style={{ fontWeight: 200 }}/>
+      </div>
+
+      <div className="mb-3">
+        <label className="form-label">Mobile Number</label>
+        <input type="number" className="form-control" placeholder="Mobile Number" style={{ fontWeight: 200 }}/>
+      </div>
+
+      <div className="mb-3">
+        <label className="form-label">Email</label>
+        <input type="email" className="form-control" placeholder="Email" style={{ fontWeight: 200 }}/>
+      </div>
+
+      <div className="mb-3 text-center">
+       <label className="form-label d-block">Gender</label>
+          <div className="d-flex justify-content-center gap-5">
+             <div className="d-flex align-items-center gap-1"> {/*Radios spacing ke liye */}
+              <input type="radio" id="male" name="gender" value="male" />
+              <label htmlFor="male" className="mb-0">Male</label>
+             </div>
+          <div className="d-flex align-items-center gap-1">
+            <input type="radio" id="female" name="gender" value="female" />
+            <label htmlFor="female" className="mb-0">Female</label>
+          </div>
+         </div>
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Create Password</label>
+        <input type="password" className="form-control" placeholder="Password" style={{ fontWeight: 200 }}/>
+      </div>
+      </form>
+      <button className="btn btn-info w-100 text-white fw-bold">Register</button>
+    </div>
+  );
+}
+export default RegisterForm
